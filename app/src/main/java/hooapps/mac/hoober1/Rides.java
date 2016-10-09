5e04ddab285632faaf09f7741ca47652fb076616
macsochor <macleansochor@gmail.com>
2016-10-09 14:56:10 -0400

    rides button goes to ridecreateform

--- app/src/main/java/hooapps/mac/hoober1/Rides.java
+++ app/src/main/java/hooapps/mac/hoober1/Rides.java
@@ -1,5 +1,6 @@
 package hooapps.mac.hoober1;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -15,7 +16,7 @@ public class Rides extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button settings, filter, newRide;
-        
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
@@ -32,7 +33,15 @@ public class Rides extends AppCompatActivity {
         newRide = (Button)findViewById(R.id.newButton);
         newRide.setBackgroundColor(0xFFFFA500);
         newRide.setTextColor(Color.BLACK);
+        newRide.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rides.this, RideCreateForm.class);
+                Rides.this.startActivity(intent);
+            }
+
+        });
 
     }
 
