a0930c4ac13704ff9f9d6f43dd4c534f102ff70c
macsochor <macleansochor@gmail.com>
2016-10-18 23:36:30 -0400

    Playing around with firebase

--- app/src/main/java/hooapps/mac/hoober1/FirstScreen.java
+++ app/src/main/java/hooapps/mac/hoober1/FirstScreen.java
@@ -4,13 +4,33 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FirstScreen extends AppCompatActivity {
-    Button signup, login;
+    Button signup, login, db;
+    EditText et;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    private DatabaseReference mDatabase;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //gets rid of title bar
@@ -20,9 +40,12 @@ public class FirstScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); //<< this
         setContentView(R.layout.activity_first_screen);
+
+        et = (EditText)findViewById(R.id.editText);
+
         //initialize buttons
-        signup = (Button)findViewById(R.id.signUpButton);
-        signup.setBackgroundColor(0xFFFFA500);
+        signup = (Button) findViewById(R.id.signUpButton);
+        signup.setBackgroundColor(0xFF00FF00);
         signup.setTextColor(Color.BLACK);
         signup.setOnClickListener(new View.OnClickListener() {
 
@@ -33,7 +56,7 @@ public class FirstScreen extends AppCompatActivity {
             }
 
         });
-        login = (Button)findViewById(R.id.loginButton);
+        login = (Button) findViewById(R.id.loginButton);
         login.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -44,5 +67,91 @@ public class FirstScreen extends AppCompatActivity {
 
         });
         login.setBackgroundColor(0xFF0099CC);
+
+        db = (Button) findViewById(R.id.db);
+        db.setBackgroundColor(0xFFFF00FF);
+        db.setTextColor(Color.BLACK);
+        db.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the databse
+
+                myRef.setValue(et.getText().toString());
+                writeNewPost(et.getText().toString(), "a", "b", "c");
+            }
+        });
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //Read from the database
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+
+//                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
+    @IgnoreExtraProperties
+    public class Post {
+
+        public String uid;
+        public String author;
+        public String title;
+        public String body;
+        public int starCount = 0;
+        public Map<String, Boolean> stars = new HashMap<>();
+
+        public Post() {
+            // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+        }
+
+        public Post(String uid, String author, String title, String body) {
+            this.uid = uid;
+            this.author = author;
+            this.title = title;
+            this.body = body;
+        }
+
+        @Exclude
+        public Map<String, Object> toMap() {
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("uid", uid);
+            result.put("author", author);
+            result.put("title", title);
+            result.put("body", body);
+            result.put("starCount", starCount);
+            result.put("stars", stars);
+
+            return result;
+        }
+
+    }
+
+    private void writeNewPost(String userId, String username, String title, String body) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("posts").push().getKey();
+        Post post = new Post(userId, username, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
+
+
 }
--- app/src/main/java/hooapps/mac/hoober1/Rides.java
+++ app/src/main/java/hooapps/mac/hoober1/Rides.java
@@ -1,21 +1,34 @@
 package hooapps.mac.hoober1;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class Rides extends AppCompatActivity {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button settings, filter, newRide;
+
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -43,6 +56,82 @@ public class Rides extends AppCompatActivity {
 
         });
 
+//        mTitle = mDrawerTitle = getTitle();
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+//                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+//
+//            /** Called when a drawer has settled in a completely closed state. */
+//            public void onDrawerClosed(View view) {
+//                super.onDrawerClosed(view);
+//                getActionBar().setTitle(mTitle);
+//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+//            }
+//
+//            /** Called when a drawer has settled in a completely open state. */
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle(mDrawerTitle);
+//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+//            }
+//        };
+//
+//        // Set the drawer toggle as the DrawerListener
+//        mDrawerLayout.setDrawerListener(mDrawerToggle);
+//    }
+//
+//    /* Called whenever we call invalidateOptionsMenu() */
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // If the nav drawer is open, hide action items related to the content view
+//        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+//        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+//        // Set the adapter for the list* view
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+//                R.layout.drawer_list_item, mPlanetTitles));
+//        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+//
+//
     }
+//
+//    @Override
+//    public void onBackPressed() {
+//    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+//    private void selectItem(int position) {
+//        // Create a new fragment and specify the planet to show based on position
+//        Fragment fragment = new PlanetFragment();
+//        Bundle args = new Bundle();
+//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        fragment.setArguments(args);
+//
+//        // Insert the fragment by replacing any existing fragment
+//        FragmentManager fragmentManager = getFragmentManager();
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.content_frame, fragment)
+//                .commit();
+//
+//        // Highlight the selected item, update the title, and close the drawer
+//        mDrawerList.setItemChecked(position, true);
+//        setTitle(mPlanetTitles[position]);
+//        mDrawerLayout.closeDrawer(mDrawerList);
+//    }
+//
+//    @Override
+//    public void setTitle(CharSequence title) {
+//        mTitle = title;
+//        getActionBar().setTitle(mTitle);
+//    }
 
 }
